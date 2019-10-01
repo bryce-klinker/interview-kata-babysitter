2d992029ebdf1128af34061b07014678504b258e
Bryce Klinker <bklinker@pillartechnology.com>
2019-10-01 15:47:51 -0500

    Adding previously commented out code to get tests back to gree

--- src/rates.js
+++ src/rates.js
@@ -18,10 +18,10 @@ export function getRatesForFamily(family) {
     {hour: 20, rate: 15},
     {hour: 21, rate: 15},
     {hour: 22, rate: 15},
-    // {hour: 23, rate: 20},
-    // {hour: 0, rate: 20},
-    // {hour: 1, rate: 20},
-    // {hour: 2, rate: 20},
-    // {hour: 3, rate: 20},
+    {hour: 23, rate: 20},
+    {hour: 0, rate: 20},
+    {hour: 1, rate: 20},
+    {hour: 2, rate: 20},
+    {hour: 3, rate: 20},
   ];
 }
