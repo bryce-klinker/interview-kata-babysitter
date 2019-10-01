273a3350740375e04bedc8f464740fbaa0bbace6
Bryce Klinker <bklinker@pillartechnology.com>
2019-10-01 15:37:39 -0500

    Adding whitespace to between act and assert

--- src/rates.spec.js
+++ src/rates.spec.js
@@ -3,6 +3,7 @@ import {getRatesForFamily} from "./rates";
 
 test('Given family A when get rates then $15 per hour before 11:00 PM', t => {
   const rates = getRatesForFamily('A');
+
   t.deepEqual({ hour: 17, rate: 15 }, rates[0]);
   t.deepEqual({ hour: 18, rate: 15 }, rates[1]);
   t.deepEqual({ hour: 19, rate: 15 }, rates[2]);
@@ -13,6 +14,7 @@ test('Given family A when get rates then $15 per hour before 11:00 PM', t => {
 
 test('Given family A when get rates then $20 per hour after 11:00 PM', t => {
   const rates = getRatesForFamily('A');
+
   t.deepEqual({ hour: 23, rate: 20 }, rates[6]);
   t.deepEqual({ hour: 0, rate: 20 }, rates[7]);
   t.deepEqual({ hour: 1, rate: 20 }, rates[8]);
