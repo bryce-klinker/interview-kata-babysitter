a03c03f68e41fa82bce4a152eac472d96e7e9f07
Bryce Klinker <bklinker@pillartechnology.com>
2019-10-01 15:05:25 -0500

    Scenario runs clean when options are used. Plumbing all works for cucumber

--- features/support/steps/payment-steps.js
+++ features/support/steps/payment-steps.js
@@ -1,18 +1,26 @@
 const { Given, When, Then,  } = require('cucumber');
 const { expect } = require('chai');
 
+let options = {
+  arrivalTime: null,
+  family: null,
+  leaveTime: null
+};
+
 Given(/^I arrive at '(.*)'$/, function (time) {
-  return 'pending';
+  options.arrivalTime = time;
 });
 
 Given(/^I am working for family '(.*)'$/, function (family) {
-  return 'pending';
+  options.family = family;
 });
 
 When(/^I leave at '(.*)'$/, function (time) {
-  return 'pending';
+  options.leaveTime = time;
 });
 
 Then(/^I should charge '(.*)' for the night$/, function (total) {
-  return 'pending';
+  expect(options.arrivalTime).to.equal('5:00 PM');
+  expect(options.leaveTime).to.equal('12:00 AM');
+  expect(options.family).to.equal('A');
 });
