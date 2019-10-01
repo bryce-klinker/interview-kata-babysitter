62b6bd4a130b806b09db37d8608431e7ed9a129a
Bryce Klinker <bklinker@pillartechnology.com>
2019-10-01 15:49:09 -0500

    Refactoring tests to use chai. Commented required production code to show failing tests using chai

--- src/rates.spec.js
+++ src/rates.spec.js
@@ -26,11 +26,11 @@ test('Given family A when get rates then $20 per hour after 11:00 PM', t => {
 test('Given family B when get rates then $12 per hour before 10:00 PM', t => {
   const rates = getRatesForFamily('B');
 
-  t.deepEqual({hour: 17, rate: 12}, rates[0]);
-  t.deepEqual({hour: 18, rate: 12}, rates[1]);
-  t.deepEqual({hour: 19, rate: 12}, rates[2]);
-  t.deepEqual({hour: 20, rate: 12}, rates[3]);
-  t.deepEqual({hour: 21, rate: 12}, rates[4]);
+  expect(rates).to.deep.include({hour: 17, rate: 12});
+  expect(rates).to.deep.include({hour: 18, rate: 12});
+  expect(rates).to.deep.include({hour: 19, rate: 12});
+  expect(rates).to.deep.include({hour: 20, rate: 12});
+  expect(rates).to.deep.include({hour: 21, rate: 12});
 });
 
 test('Given family B when get rates then $8 per hour between 10:00 PM and 12:00 AM', t => {
