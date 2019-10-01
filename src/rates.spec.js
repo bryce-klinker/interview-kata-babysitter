1e5c93b62cabfa35681b15d5d3c04d67e9bf5963
Bryce Klinker <bklinker@pillartechnology.com>
2019-10-01 15:44:18 -0500

    Starting to change code to use chai assertions as they will not enforce array location equality

--- src/rates.spec.js
+++ src/rates.spec.js
@@ -1,41 +1,42 @@
 import test from 'ava';
+import {expect} from 'chai';
 import {getRatesForFamily} from "./rates";
 
 test('Given family A when get rates then $15 per hour before 11:00 PM', t => {
   const rates = getRatesForFamily('A');
-
-  t.deepEqual({ hour: 17, rate: 15 }, rates[0]);
-  t.deepEqual({ hour: 18, rate: 15 }, rates[1]);
-  t.deepEqual({ hour: 19, rate: 15 }, rates[2]);
-  t.deepEqual({ hour: 20, rate: 15 }, rates[3]);
-  t.deepEqual({ hour: 21, rate: 15 }, rates[4]);
-  t.deepEqual({ hour: 22, rate: 15 }, rates[5]);
+  expect(rates).to.deep.include({hour:17, rate: 15});
+  // t.deepEqual({hour: 17, rate: 15}, rates[0]);
+  t.deepEqual({hour: 18, rate: 15}, rates[1]);
+  t.deepEqual({hour: 19, rate: 15}, rates[2]);
+  t.deepEqual({hour: 20, rate: 15}, rates[3]);
+  t.deepEqual({hour: 21, rate: 15}, rates[4]);
+  t.deepEqual({hour: 22, rate: 15}, rates[5]);
 });
 
 test('Given family A when get rates then $20 per hour after 11:00 PM', t => {
   const rates = getRatesForFamily('A');
 
-  t.deepEqual({ hour: 23, rate: 20 }, rates[6]);
-  t.deepEqual({ hour: 0, rate: 20 }, rates[7]);
-  t.deepEqual({ hour: 1, rate: 20 }, rates[8]);
-  t.deepEqual({ hour: 2, rate: 20 }, rates[9]);
-  t.deepEqual({ hour: 3, rate: 20 }, rates[10]);
+  t.deepEqual({hour: 23, rate: 20}, rates[6]);
+  t.deepEqual({hour: 0, rate: 20}, rates[7]);
+  t.deepEqual({hour: 1, rate: 20}, rates[8]);
+  t.deepEqual({hour: 2, rate: 20}, rates[9]);
+  t.deepEqual({hour: 3, rate: 20}, rates[10]);
 });
 
 test('Given family B when get rates then $12 per hour before 10:00 PM', t => {
   const rates = getRatesForFamily('B');
 
-  t.deepEqual({ hour: 17, rate: 12 }, rates[0]);
-  t.deepEqual({ hour: 18, rate: 12 }, rates[1]);
-  t.deepEqual({ hour: 19, rate: 12 }, rates[2]);
-  t.deepEqual({ hour: 20, rate: 12 }, rates[3]);
-  t.deepEqual({ hour: 21, rate: 12 }, rates[4]);
+  t.deepEqual({hour: 17, rate: 12}, rates[0]);
+  t.deepEqual({hour: 18, rate: 12}, rates[1]);
+  t.deepEqual({hour: 19, rate: 12}, rates[2]);
+  t.deepEqual({hour: 20, rate: 12}, rates[3]);
+  t.deepEqual({hour: 21, rate: 12}, rates[4]);
 });
 
 test('Given family B when get rates then $8 per hour between 10:00 PM and 12:00 AM', t => {
   const rates = getRatesForFamily('B');
 
-  t.deepEqual({ hour: 22, rate: 8 }, rates[5]);
-  t.deepEqual({ hour: 23, rate: 8 }, rates[6]);
+  t.deepEqual({hour: 22, rate: 8}, rates[5]);
+  t.deepEqual({hour: 23, rate: 8}, rates[6]);
 });
 
