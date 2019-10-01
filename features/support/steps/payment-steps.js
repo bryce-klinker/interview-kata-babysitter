c2297a98736b13ac6d402c140d4c9847c6d96c2d
Bryce Klinker <bklinker@pillartechnology.com>
2019-10-01 15:11:32 -0500

    Added babel to allow es6 style imports/exports

--- features/support/steps/payment-steps.js
+++ features/support/steps/payment-steps.js
@@ -1,11 +1,13 @@
-const { Given, When, Then,  } = require('cucumber');
-const { expect } = require('chai');
+const {Given, When, Then,} = require('cucumber');
+const {expect} = require('chai');
+const {calculatePayment} = require('../../../src/baby-sitter');
 
-let options = {
+const options = {
   arrivalTime: null,
   family: null,
   leaveTime: null
 };
+let actualTotal = null;
 
 Given(/^I arrive at '(.*)'$/, function (time) {
   options.arrivalTime = time;
@@ -17,10 +19,9 @@ Given(/^I am working for family '(.*)'$/, function (family) {
 
 When(/^I leave at '(.*)'$/, function (time) {
   options.leaveTime = time;
+  actualTotal = calculatePayment(options);
 });
 
 Then(/^I should charge '(.*)' for the night$/, function (total) {
-  expect(options.arrivalTime).to.equal('5:00 PM');
-  expect(options.leaveTime).to.equal('12:00 AM');
-  expect(options.family).to.equal('A');
+  expect(actualTotal).to.equal(total);
 });
---
+++ src/baby-sitter.js
@@ -0,0 +1,3 @@
+export function calculatePayment() {
+  return null;
+}
