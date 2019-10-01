c4c4f1ee1b9bc1f63951d25923ae92b9add13601
Bryce Klinker <bklinker@pillartechnology.com>
2019-10-01 15:37:14 -0500

    Updated to start asserting on rates for family 'B'

--- src/rates.spec.js
+++ src/rates.spec.js
@@ -20,3 +20,13 @@ test('Given family A when get rates then $20 per hour after 11:00 PM', t => {
   t.deepEqual({ hour: 3, rate: 20 }, rates[10]);
 });
 
+test('Given family B when get rates then $12 per hour before 10:00 PM', t => {
+  const rates = getRatesForFamily('B');
+
+  t.deepEqual({ hour: 17, rate: 12 }, rates[0]);
+  t.deepEqual({ hour: 18, rate: 12 }, rates[1]);
+  t.deepEqual({ hour: 19, rate: 12 }, rates[2]);
+  t.deepEqual({ hour: 20, rate: 12 }, rates[3]);
+  t.deepEqual({ hour: 21, rate: 12 }, rates[4]);
+});
+
