0899325c2e3a63e8743b8fbc4132e9c3b47a3ea1
Bryce Klinker <bklinker@pillartechnology.com>
2019-10-02 08:54:32 -0500

    Refactoring hours to make getHoursWorked more clear

--- src/hours.js
+++ src/hours.js
@@ -1,19 +1,22 @@
-export function getHoursWorked(startTime, endTime) {
-  let currentHour = getHour(startTime);
-  if (currentHour < 17) {
-    throw `A start time of ${startTime} is not allowed`;
-  }
-  const endHour = getHour(endTime);
-
+function collectHoursWorked(startHour, endHour) {
   const hoursWorked = [];
-  while(currentHour !== endHour) {
-    hoursWorked.push(currentHour);
-    currentHour++;
-    if (currentHour === 24) {
-      currentHour = 0;
+  while (startHour !== endHour) {
+    hoursWorked.push(startHour);
+    startHour++;
+    if (startHour === 24) {
+      startHour = 0;
     }
   }
   return hoursWorked;
+}
+
+export function getHoursWorked(startTime, endTime) {
+  const startHour = getHour(startTime);
+  if (startHour < 17) {
+    throw `A start time of ${startTime} is not allowed`;
+  }
+  const endHour = getHour(endTime);
+  return collectHoursWorked(startHour, endHour);
 }
 
 export function getHour(time) {
