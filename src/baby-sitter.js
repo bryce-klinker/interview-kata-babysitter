cb506b4f459523f41c2b18a961ec1be4a289c4e6
Bryce Klinker <bklinker@pillartechnology.com>
2019-10-02 09:26:39 -0500

    Added 'shell' code to baby sitter to perform payment calculation

--- src/baby-sitter.js
+++ src/baby-sitter.js
@@ -1,3 +1,9 @@
-export function calculatePayment() {
-  return null;
+import {getRatesForFamily} from "./rates";
+import {getHoursWorked} from "./hours";
+import {calculatePaymentAmount} from "./payment-calculator";
+
+export function calculatePayment({ family, endTime, startTime}) {
+  const rates = getRatesForFamily(family);
+  const hoursWorked = getHoursWorked(startTime, endTime);
+  return calculatePaymentAmount(hoursWorked, rates);
 }
