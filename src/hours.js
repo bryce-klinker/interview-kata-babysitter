2532b61cccac92756c72e6cb1486667ea4d728a6
Bryce Klinker <bklinker@pillartechnology.com>
2019-10-02 08:00:53 -0500

    Updated to have hours for 7:00 PM

--- src/hours.js
+++ src/hours.js
@@ -1,3 +1,6 @@
-export function getHoursWorked() {
+export function getHoursWorked(startTime) {
+  if (startTime === '7:00 PM') {
+    return [18, 19, 20, 21, 22, 23];
+  }
   return [17, 18, 19, 20, 21, 22];
 }
--- src/hours.spec.js
+++ src/hours.spec.js
@@ -12,3 +12,13 @@ test('When start time is 5:00 PM and end time is 11:00 PM then hours between 5:0
   expect(hoursWorked).to.include(21);
   expect(hoursWorked).to.include(22);
 });
+
+test('When star time is 7:00 PM and end time is 12:00 AM then hours between 7:00 PM and 12:00 AM returned', () => {
+  const hoursWorked = getHoursWorked('7:00 PM', '12:00 AM');
+
+  expect(hoursWorked).to.include(19);
+  expect(hoursWorked).to.include(20);
+  expect(hoursWorked).to.include(21);
+  expect(hoursWorked).to.include(22);
+  expect(hoursWorked).to.include(23);
+})
