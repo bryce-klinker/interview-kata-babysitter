7dbc01ff25b024d59a43398352e872c764400f8d
Bryce Klinker <bklinker@pillartechnology.com>
2019-10-02 09:17:20 -0500

    Updating to use a constant for fifteen dollar flat rate rates

--- src/payment-calculator.spec.js
+++ src/payment-calculator.spec.js
@@ -2,22 +2,23 @@ import test from 'ava';
 import {expect} from 'chai';
 import {calculatePaymentAmount} from "./payment-calculator";
 
+const fifteenDollarFlatRate = [
+  {hour: 17, rate: 15},
+  {hour: 18, rate: 15},
+  {hour: 19, rate: 15},
+  {hour: 20, rate: 15},
+  {hour: 21, rate: 15},
+  {hour: 22, rate: 15},
+  {hour: 23, rate: 15},
+  {hour: 0, rate: 15},
+  {hour: 1, rate: 15},
+  {hour: 2, rate: 15},
+  {hour: 3, rate: 15},
+];
+
 test('When one hour worked and rate is $15 per hour then payment is $15', () => {
   const hoursWorked = [17];
-  const rates = [
-    {hour: 17, rate: 15},
-    {hour: 18, rate: 15},
-    {hour: 19, rate: 15},
-    {hour: 20, rate: 15},
-    {hour: 21, rate: 15},
-    {hour: 22, rate: 15},
-    {hour: 23, rate: 15},
-    {hour: 0, rate: 15},
-    {hour: 1, rate: 15},
-    {hour: 2, rate: 15},
-    {hour: 3, rate: 15},
-  ];
-  const payment = calculatePaymentAmount(hoursWorked, rates);
+  const payment = calculatePaymentAmount(hoursWorked, fifteenDollarFlatRate);
 
   expect(payment).to.equal('$15');
 });
