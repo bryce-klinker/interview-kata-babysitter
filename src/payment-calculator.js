b1ca586c1b5f059669618bf77cee9168126b5327
Bryce Klinker <bklinker@pillartechnology.com>
2019-10-02 09:24:32 -0500

    The calculation now uses rates and hours worked

--- src/payment-calculator.js
+++ src/payment-calculator.js
@@ -1,4 +1,5 @@
-export function calculatePaymentAmount(hoursWorked) {
-  const total = 15 * hoursWorked.length;
+export function calculatePaymentAmount(hoursWorked, rates) {
+  const ratesWorked = rates.filter(r => hoursWorked.includes(r.hour));
+  const total = ratesWorked.reduce((p, r) => p + r.rate, 0);
   return `$${total}`;
 }
--- src/payment-calculator.spec.js
+++ src/payment-calculator.spec.js
@@ -31,3 +31,25 @@ test('When two hours worked and rate is $15 per hour then payment is $30', () =>
 
   expect(payment).to.equal('$30');
 });
+
+test('When one hour worked at $15 and second hour worked at $30 then payment is $45', () => {
+  const hoursWorked = [17, 18];
+  const rates = [
+    {hour: 17, rate: 15},
+    {hour: 18, rate: 30},
+    {hour: 19, rate: 30},
+    {hour: 20, rate: 30},
+    {hour: 21, rate: 30},
+    {hour: 22, rate: 30},
+    {hour: 23, rate: 30},
+    {hour: 0, rate: 30},
+    {hour: 1, rate: 30},
+    {hour: 2, rate: 30},
+    {hour: 3, rate: 30},
+  ];
+
+  const payment = calculatePaymentAmount(hoursWorked, rates);
+
+  expect(payment).to.equal('$45');
+
+});
